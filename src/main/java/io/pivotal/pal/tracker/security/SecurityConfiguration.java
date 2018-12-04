1df605ddd441064058832f59698b09074b031b07
karan_k@dell.com <karan_k@dell.com>
2018-12-04 12:33:59 -0700

    security in progress

---
+++ src/main/java/io/pivotal/pal/tracker/security/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package io.pivotal.pal.tracker.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.xml.ws.WebServiceException;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    
+}
